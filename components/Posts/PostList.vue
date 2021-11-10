aa2e9d1af6e216cf5d0ea5afe6b07c4a0d1bf76b
hestus <hestushestus@gmail.com>
2021-11-10 22:14:23 +0530

    first commit

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,51 @@
+<template>
+    <section class="post-list">
+      <PostPreview
+        id="1"
+        :is-admin="isAdmin"
+        title="Hello First post"
+        thumbnail="https://about.fb.com/wp-content/uploads/2021/10/Meta-Planets-img-16x9-1.jpg?w=1200"
+        previewText="This is my first post!"
+      />
+      <PostPreview
+        id="2"
+        :is-admin="isAdmin"
+        title="Hello Second Post"
+        thumbnail="https://about.fb.com/wp-content/uploads/2021/10/Meta-Planets-img-16x9-1.jpg?w=1200"
+        previewText="This is my second post!"
+      />
+      <PostPreview
+        id="3"
+        :is-admin="isAdmin"
+        title="Hello Third Post"
+        thumbnail="https://about.fb.com/wp-content/uploads/2021/10/Meta-Planets-img-16x9-1.jpg?w=1200"
+        previewText="This is my third post!"
+      />
+    </section>
+</template>
+
+<script>
+import PostPreview from "@/components/Posts/PostPreview.vue";
+export default {
+    components: {
+    PostPreview,
+  },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    }
+  }
+}
+</script>
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost" />
+    </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from "~/components/Admin/AdminPostForm.vue";
+
+export default {
+  components: { AdminPostForm },
+
+  data() {
+    return {
+      loadedPost: {
+        author: "Manas Mishra",
+        title: "My awesome Post",
+        content: "Super Amazing, thanks for that",
+        thumbnailLink:
+          "https://about.fb.com/wp-content/uploads/2021/10/Meta-Planets-img-16x9-1.jpg?w=1200",
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
